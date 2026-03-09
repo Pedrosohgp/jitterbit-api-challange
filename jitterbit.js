3d691db064025ae54098b0f8aed640c6c2b6cbc3
Henrique Pedroso <gomespedrosoh@gmail.com>
2026-03-09 16:33:12 -0300

    Commit inicial - Estrutura base da API

---
+++ jitterbit.js
@@ -0,0 +1,12 @@
+//Exercício 9
+function somaImpares(n) {
+    let soma = 0;
+    for(let i = 1; i<=n; i++) {
+        if(i % 2 !== 0) {
+            soma += i;
+        }
+    }
+    return soma;
+}
+
+console.log(somaImpares(7))
